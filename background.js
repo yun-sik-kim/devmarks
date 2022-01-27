82452dade1b1733647bc16587043968e5a8b5a2e
Yunsik Kim <imaginebuck02@gmail.com>
2022-01-28 01:14:31 +0900

    constructed indexDB to store data

--- background.js
+++ background.js
@@ -1,17 +1,51 @@
 "use strict";
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "insert") {
+    let insert_request = insert_bookmark(request.payload);
+    insert_request.then((res) => {
+      chrome.runtime.sendMessage({
+        message: "insert_success",
+        payload: res,
+      });
+    });
+  } else if (request.message === "get") {
+    let get_request = get_bookmark(request.payload);
+    get_request.then((res) => {
+      chrome.runtime.sendMessage({
+        message: "get_success",
+        payload: res,
+      });
+    });
+  } else if (request.message === "update") {
+    let update_request = update_bookmark(request.payload);
+    update_request.then((res) => {
+      chrome.runtime.sendMessage({
+        message: "update_success!",
+        payload: res,
+      });
+    });
+  } else if (request.message === "delete") {
+    let delete_request = delete_bookmark(request.payload);
+    delete_request.then((res) => {
+      chrome.runtime.sendMessage({
+        message: "delete_success!",
+        payload: res,
+      });
+    });
+  }
+});
 
 let bookmark = [
-{
-    "nickname": "MDN Web Docs",
-    "url": "https://developer.mozilla.org/en-US/"
-},
-{
-    "nickname": "Google fonts",
-    "url": "https://fonts.google.com/"
-}
-]
+  {
+    nickname: "MDN Web Docs",
+    url: "https://developer.mozilla.org/en-US/",
+  },
+  {
+    nickname: "Google fonts",
+    url: "https://fonts.google.com/",
+  },
+];
 
 let db = null;
 
@@ -34,43 +68,157 @@ let db = null;
 //   }
 // }
 
+// const request = window.indexedDB.open("test");
+// this.url = url;
+// this.nickname = nickname;
+// this.time = time;
+// this.Tags = Tags;
+
+// return Bookmark;
+
 function create_database() {
-    const request = window.indexedDB.open('BookMarkDB', 3);
+  var request = self.indexedDB.open("extensionDB");
 
-    request.onerror = function(event) {
-        console.log("ERROR, DB cannot be opened")
-    }
-    request.onupgradeneeded = function(event) {
-        db = event.target.results;
-        let object_store = db.created_object_store('bookmark'. {
-            keyPath: 'url'
-        });
+  request.onerror = function (event) {
+    console.log("ERROR, DB cannot be opened");
+  };
 
-    }
-    request.onsuccess = function(event) {
+  request.onupgradeneeded = function (event) {
+    // db = event.target.results;
+    db = request.result;
 
-    }
+    let objectStore = db.createObjectStore("bookmark", {
+      keyPath: "url",
+    });
 
+    objectStore.transaction.oncomplete = function (event) {
+      console.log("objectStore created!");
+    };
+  };
 
+  request.onsuccess = function (event) {
+    db = event.target.result;
+    console.log("DB opened!");
+    // insert_bookmark(bookmark);
+
+    // db.onerror = function (event) {
+    //   console.log("FAILED TO OPEN DB.");
+    // };
+  };
 }
 
-function record_bookmark() {
-  const request = window.indexedDB.open("test");
-  this.url = url;
-  this.nickname = nickname;
-  this.time = time;
-  this.Tags = Tags;
+function delete_database() {
+  const request = window.indexedDB.open("BookMarkDB");
 
-  return Bookmark;
+  request.onerror = function (event) {
+    console.log("ERROR, failed to delete DB");
+  };
+
+  request.onsuccess = function (event) {
+    db = event.target.result;
+    console.log("db deleted!");
+  };
 }
 
-function delete_bookmark() {}
+function insert_bookmark(url) {
+  if (db) {
+    const insert_transaction = db.transaction("bookmark", "readwrite");
+    const objectStore = insert_transaction.objectStore("bookmark");
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
+    return new Promise((resolve, reject) => {
+      insert_transaction.oncomplete = function () {
+        console.log("All insert transactions complete!");
+        resolve(true);
+      };
+
+      insert_transaction.onerror = function () {
+        console.log("ERROR! problem inserting records");
+        resolve(false);
+      };
+
+      url.forEach((value) => {
+        let request = objectStore.add(value);
+
+        request.onsuccess = function () {
+          console.log("Added: ", value);
+        };
+      });
+    });
   }
-});
+}
+
+function get_bookmark(url) {
+  if (db) {
+    const get_transaction = db.transaction("bookmark", "readonly");
+    const objectStore = get_transaction.objectStore("bookmark");
+
+    return new Promise((resolve, reject) => {
+      get_transaction.oncomplete = function () {
+        console.log("All get transactions complete!");
+      };
+
+      get_transaction.onerror = function () {
+        console.log("ERROR! problem getting records");
+      };
+
+      let request = objectStore.get(url);
+
+      request.onsuccess = function (event) {
+        resolve(event.target.result);
+      };
+    });
+  }
+}
+
+function update_bookmark(url) {
+  if (db) {
+    const put_transaction = db.transaction("bookmark", "readwrite");
+    const objectStore = put_transaction.objectStore("bookmark");
+
+    return new Promise((resolve, reject) => {
+      put_transaction.oncomplete = function () {
+        console.log("All put transactions complete!");
+        resolve(true);
+      };
+
+      put_transaction.onerror = function () {
+        console.log("ERROR! problem updating records");
+        resolve(false);
+      };
+
+      objectStore.put(url);
+    });
+  }
+}
+
+function delete_bookmark(url) {
+  if (db) {
+    const del_transaction = db.transaction("bookmark", "readwrite");
+    const objectStore = del_transaction.objectStore("bookmark");
+
+    return new Promise((resolve, reject) => {
+      del_transaction.oncomplete = function () {
+        console.log("All delete transactions complete!");
+        resolve(true);
+      };
+
+      del_transaction.onerror = function () {
+        console.log("ERROR! problem deleting records");
+        resolve(false);
+      };
+
+      objectStore.delete(url);
+    });
+  }
+}
+
+// chrome.storage.onChanged.addListener(function (changes, namespace) {
+//   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+//     console.log(
+//       `Storage key "${key}" in namespace "${namespace}" changed.`,
+//       `Old value was "${oldValue}", new value is "${newValue}".`
+//     );
+//   }
+// });
+
+create_database();
--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 "use strict"; // used in Valina Javascript. Used to restrict "flexibility" of JS ex)allocate value to not initialized variable
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "insert_success") {
+    // if (request.payload) {
+    //   add_button.}
+    console.log("success!");
+  } else if (request.message === "get_success") {
 
+  } else if (request.message === "update_success") {
+  } else if (request.message === "delete_success") {
+  }
 });
-
 
 const background = document.querySelector(".background");
 const dir_button = document.querySelector("#dir_button");
@@ -14,35 +21,53 @@ const close_buttons = [
 const save_button = document.querySelector(".save_button");
 const add_button = document.querySelector("#add_button");
 
+var title;
+var address;
+
 
 
 dir_button.addEventListener("click", () => {
   background.classList.add("show");
 });
 
-close_buttons.forEach(c_btn => {
+close_buttons.forEach((c_btn) => {
   c_btn.addEventListener("click", () => {
     background.classList.remove("show");
   });
 });
 
-add_button.addEventListener("click", () => {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    // console.log(tabs[0]);
-    console.log(tabs[0].title);
-    console.log(tabs[0].url);
-    // console.log(tabs[0].favIconUrl);
-
+add_button.addEventListener("click", (event) => {
+  event.preventDefault;
+  // const form_data = new FormData(bookmark);
+  chrome.runtime.sendMessage({
+    message: "insert",
+    payload: [
+      {
+        nickname: title,
+        url: address,
+      },
+    ],
   });
 });
 
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  // console.log(tabs[0]);
+  console.log(tabs[0].title);
+  console.log(tabs[0].url);
+  title = tabs[0].title;
+  address = tabs[0].url;
+  // console.log(tabs[0].favIconUrl);
+});
 
-
-
+array.forEach(element => {
+  
+});
+var table = document.getElementsByTagName("table")[0];
+var new_row = table.insertRow(1); // add empty row to the table below the title
+var insert_cell = new_row.insertCell(0); // fill the empty row with cell
+insert_cell.innerHTML = title; //add cell
 
 //receive the url of the current page
-
-
 
 // document.querySelector('add_button').style.background.linear-gradient(175deg, var(--highlight1) 65%, #FEF9EF 45%);
 
